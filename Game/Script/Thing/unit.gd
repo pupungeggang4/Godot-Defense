7f057335196ed189a2af7913e2a8782875614b5c
pupungeggang6 <pupungeggang6@gmail.com>
2025-05-12 01:08:26 +0900

    Reset

--- Game/Script/Thing/unit.gd
+++
@@ -1,7 +0,0 @@
-extends Node2D
-
-@export var side = 1
-@export var speed = 20.0
-
-func _process(delta: float) -> void:
-    position.x -= speed * delta
--- Game/Script/UIHandle/ui.gd
+++
@@ -1,6 +0,0 @@
-extends Node
-
-const defense = {
-    'button_upgrade': [40, 680, 80, 80],
-    'button_menu': [1160, 40, 80, 80],
-}
