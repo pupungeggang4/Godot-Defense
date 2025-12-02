191936d170015cca51bd4d3d6ed915023cecadf1
pupungeggang2 <kyongkyong2892@gmail.com>
2025-12-03 00:57:47 +0900

    Handling text

--- game/script/thing/player.gd
+++ game/script/thing/player.gd
@@ -3,3 +3,5 @@ extends Node2D
 var level: int = 1
 var energy = 0.0
 var energy_max = 2.0
+var life = 20
+var life_max = 20
--- game/script/window/ui_lower.gd
+++ game/script/window/ui_lower.gd
@@ -3,7 +3,9 @@ extends Control
 @onready var player = get_tree().current_scene.get_node('Player')
 @onready var text_level = get_node('TextLevel')
 @onready var text_energy = get_node('TextEnergy')
+@onready var text_life = get_node('TextLife')
 
 func _process(delta):
     text_level.text = 'Lv.%d' % [player.level]
     text_energy.text = '%.1f/%.1f' % [player.energy, player.energy_max]
+    text_life.text = '%d/%d' % [player.life, player.life_max]
